650b7abb773bbc2e5ee552bf73622c2d11af56ce
YenySan <yeneysanchez91@gmail.com>
2023-06-18 14:45:12 -0600

    testingJava

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 var modo = document.getElementById("modo");
-modo.onclick = function(){
-    document.body.classList.toggle("dark-mode");
-    if(document.body.classList.contains("dark-mode")){
-    modo.src="./assets/sol.png";}
-    else{
-       modo.src="./assets/luna.png";
-    }
-}
+modo.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    modo.src = "./assets/sol.png";
+  } else {
+    modo.src = "./assets/luna.png";
+  }
+};
 
-const menuIcon = document.getElementById('menu-icon');
-const menu = document.getElementById('menu');
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
 
-menuIcon.addEventListener('click', function () {
-    menu.classList.toggle('active');
+menuIcon.addEventListener("click", function () {
+  menu.classList.toggle("active");
 });
-
