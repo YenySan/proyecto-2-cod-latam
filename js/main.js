066ce04bc26fb2e05639fae15f690d7763a073e6
YenySan <yeneysanchez91@gmail.com>
2023-06-28 19:08:30 -0600

    foto

--- js/main.js
+++ js/main.js
@@ -15,4 +15,11 @@ menuIcon.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 
+const listItem = document.querySelectorAll(".item");
 
+listItem.forEach((itemLi) => {
+  itemLi.addEventListener("click", (evento) => {});
+  const activoItem=document.querySelector(".activo");
+  activoItem.classList.remove("activo");
+  evento.target.classList.add("activo");
+});
